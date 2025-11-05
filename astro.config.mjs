3cc75b0f269c68feb97faaefd33deb6bb07fdbdb
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2025-11-05 10:44:49 +0100

    feat: enable SSR support with Node adapter in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node"; // AJOUT
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server", // AJOUT => active le mode SSR pour build server
+  adapter: node({
+    // AJOUT => adapter Node SSR
+    mode: "standalone", // recommandé
+  }),
 });
